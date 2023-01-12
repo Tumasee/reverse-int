eabba02e808b4f2a3497fce39fda8d13fd1fd7a9
Tumasee <tymas-geni@mail.ru>
2023-01-12 14:57:34 +0300

    again

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let newString = '';
+    const intToString = n.toString();
+
+    for (let i = intToString.length - 1; i >= 0; i -= 1) {
+        newString += intToString[i];
+    }
+
+    return parseInt(newString) * 1;
 }
